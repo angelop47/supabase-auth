efdb06510e00067a7fa553baaa4f7d137f4ae5dc
angelop47 <pescettoangelo@gmail.com>
2025-12-29 20:10:52 -0300

    feat(server): configure CORS to allow specific origin and credentials

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,12 @@ import userRoutes from './routes/users.routes';
 
 const app = express();
 
-app.use(cors());
+app.use(cors(
+    {
+        origin: 'http://localhost:5173',
+        credentials: true,
+    }
+));
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
